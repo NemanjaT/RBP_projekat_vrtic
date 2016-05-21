896cc1a1b8693554e531cc514524f25fdfed2424
Nemanja Tozic <nemanjat94@hotmail.com>
2016-05-21 20:43:28 +0200

    Update test_skripte.sql

--- backend/test_skripte.sql
+++ backend/test_skripte.sql
@@ -1,3 +1,10 @@
+-- NE POKRECI OVE SKRIPTE
+-- DO NOT RUN THESE SCRIPTS
+-- laufen nicht diese Skripte
+-- dili modagan niini nga mga sinulatan
+-- no ejecute estas secuencias de comandos
+-- nie uruchamiać te skrypty
+
 begin
 	declare @string nchar(600);
 	set @string = 
