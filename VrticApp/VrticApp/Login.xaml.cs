c419e30e5d5dd0f45044024e7a9d309e9e80f414
milos94 <a.milos94@gmail.com>
2016-05-26 08:50:39 +0200

    App
    Aplikacija za vrtić.
    Za sad samo login i wondow za korisnika koji prikazuje informacije iz
    viewa.
    Login će morati da se menja sad radi bez user name i password.

---
+++ VrticApp/VrticApp/Login.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VrticApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogIn_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnectionStringBuilder connectionString = new SqlConnectionStringBuilder();
+            connectionString.UserID = txtName.Text;
+            connectionString.Password = txtPassword.Password;
+            connectionString.DataSource = "localhost";
+            connectionString.InitialCatalog = "VRTIC";
+            connectionString.IntegratedSecurity = true;
+            SqlConnection conn = new SqlConnection(connectionString.ToString());
+            try
+            {
+                string str = "";
+                conn.Open();
+                SqlCommand myComm = new SqlCommand("SELECT * FROM TAB_GRUPE", conn);
+                SqlDataReader reader = myComm.ExecuteReader();
+                while (reader.Read())
+                {
+                    str += reader["ID"].ToString() + ' ' + reader["IME_GRUPE"].ToString();
+                    MessageBox.Show(str);
+                }
+                reader.Close();
+            }
+            catch (Exception except) { MessageBox.Show(except.ToString()); }
+            
+            MessageBox.Show("Uspešno povezivanje sa bazom!");
+            switch (txtName.Text)
+            {
+                case "DB_KORISNIK": UserWindow usr = new UserWindow(conn); usr.Show(); this.Close(); break;
+                case "DB_ADMIN": break;
+            }
+         
+            
+
+        }
+        }
+}
---
+++ VrticApp/VrticApp/UserWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.ComponentModel;
+
+namespace VrticApp
+{
+    /// <summary>
+    /// Interaction logic for UserWindow.xaml
+    /// </summary>
+    public partial class UserWindow : Window
+    {
+        SqlDataReader reader;
+        SqlConnection conn;
+        SqlCommand command;
+        public UserWindow(SqlConnection conn)
+        {
+            InitializeComponent();
+            this.conn = conn;
+            reader = null;
+            this.Closing += OnWindowClosing;
+            ReadData("VW_DECA");
+        }
+
+        private void btnDeca_Click(object sender, RoutedEventArgs e)
+        {
+            ReadData("VW_DECA");
+        }
+        private void btnGrupe_Click(object sender, RoutedEventArgs e)
+        {
+            ReadData("VW_GRUPE");
+        }
+        private void btnStaratelji_Click(object sender, RoutedEventArgs e)
+        {
+            ReadData("VW_STARATELJI");
+        }
+        private void btnStaratelji_Deca_Click(object sender, RoutedEventArgs e)
+        {
+            ReadData("VW_STARATELJI_DECA");
+        }
+        private void btnLogOut_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ReadData(string Table)
+        {
+            listView.Items.Clear();
+            try
+            {
+                string str = "";
+                command = new SqlCommand("SELECT * FROM "+Table , conn);
+                reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    foreach (var x in reader)
+                        str += x.ToString() + '\t';
+                    listView.Items.Add(str);
+                }
+            }
+            catch (Exception exc) { MessageBox.Show(exc.ToString()); }
+            reader.Close();
+            command = null;
+        }
+
+        public void OnWindowClosing(object sender, CancelEventArgs e)
+        {
+            try
+            {
+                conn.Close();
+                reader.Close();
+                command = null;
+            }
+            catch (Exception excp) { MessageBox.Show(excp.ToString()); }
+            MainWindow log = new MainWindow();
+            log.Show();
+        }
+
+
+    }
+
+}
