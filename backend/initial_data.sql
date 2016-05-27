80f79e0f53de8b3d5f19bdae79fed8433a47bc2f
Nemanja Tozic <nemanjat94@hotmail.com>
2016-05-27 18:54:37 +0200

    Fixed insert error.

--- backend/initial_data.sql
+++ backend/initial_data.sql
@@ -10,10 +10,13 @@ begin
 end
 go
 begin
+	set identity_insert TAB_GRUPE on
 	insert into TAB_GRUPE (ID, IME_GRUPE, SPRAT, VASPITACI, CENA)
 	values (1000, 'Cvetici', 2, 'Dragana Mitic, Snezana Savic', 3000.0),
 		   (1001, 'Autici', 1, 'Jagorka Stamonasivljevic Jovanovic, John Doe, Foo Bar', 3200.0);
+	set identity_insert TAB_GRUPE off
 
+	set identity_insert TAB_DECA on
 	insert into TAB_DECA (ID, IME, PREZIME, DATUM_RODJENJA, GRUPA_ID, DODATNE_INFORMACIJE)
 	values (1000, 'Kika', 'Simic', CAST('01/01/2010' as date), 1000, ''),
 		   (1001, 'Milja', 'Trifunovic', CAST('12/31/2009' as date), 1000, 'Cerka Sinise Trifunovica'),
@@ -26,7 +29,9 @@ begin
 		   (1008, 'Nikola', 'Tesla', CAST('09/28/2011' as date), 1001, 'Sokantan decko...'),
 		   (1009, 'Stevan', 'Sindjelic', CAST('03/02/2009' as date), 1001, ''),
 		   (1010, 'Sasa', 'Matic', CAST('10/26/2008' as date), 1001, 'Slabiji vid');
+	set identity_insert TAB_DECA off
 
+	set identity_insert TAB_FINANSIJSKE_GRUPE on
 	insert into TAB_FINANSIJSKE_GRUPE (ID, IME_GRUPE, FINANSIJSKI_OD, FINANSIJSKI_DO, DODATNE_INFORMACIJE)
 	values (1000, 'Bez primanja', 0.0, 5000.0, ''),
 		   (1001, 'Slabija primanja', 5000.01, 15000.0, ''),
@@ -34,7 +39,9 @@ begin
 		   (1003, 'Jaca primanja', 30000.01, 60000.0, ''),
 		   (1004, 'Premium', 60000.01, 150000.0, ''),
 		   (1005, 'Gold', 150000.01, 500000.0, '');
+	set identity_insert TAB_FINANSIJSKE_GRUPE off
 	
+	set identity_insert TAB_STARATELJI on
 	insert into TAB_STARATELJI (ID, IME, PREZIME, JMBG, U_RADNOM_ODNOSU, FINANSIJSKA_GRUPA_ID, DODATNE_INFORMACIJE, STARATELJ_ID)
 	values (1000, 'Sinisa', 'Trifunovic', '123456789123', 1, 1005, '', null),
 		   (1001, 'Milos', 'Andjelkovic', '123456789012', 1, 1004, '', null),
@@ -43,7 +50,9 @@ begin
 		   (1004, 'Ivana', 'Andjelkovic', '456789012345', 1, 1002, 'Dobio je kao rodjendanski poklon', 1001),
 		   (1005, 'Igor', 'Nikolic', '57890123456', 99, null, '', null),
 		   (1006, 'Svetozar', 'Sindjelic', 1, 1000, null, '', null);
+	set identity_insert TAB_STARATELJI off
 
+	set identity_insert TAB_STARATELJI_DECA on
 	insert into TAB_STARATELJI_DECA (ID, DETE_ID, STARATELJ_ID, ODNOS)
 	values (1000, 1001, 1000, 'Otac'),
 		   (1001, 1001, 1003, 'Majka'),
@@ -57,8 +66,11 @@ begin
 		   (1009, 1008, 1006, 'Slusalac'),
 		   (1010, 1009, 1006, 'Otac'),
 		   (1011, 1000, 1004, 'Tetka');
+	set identity_insert TAB_STARATELJI_DECA off
 
+	set identity_insert TAB_INCIDENTI on
 	insert into TAB_INCIDENTI (ID, DETE_ID, FINANSIJSKA_ODSTETA, PRISUTNI_VASPITAC, OPIS_INCIDENTA)
 	values (1000, 1001, 30000.0, 'Veljko Domar', 'Gurnula klavir kroz prozor.'),
 		   (1001, 1008, 15000.0, 'Stasa Kuvarica', 'Zapalio osigurac');
+	set identity_insert TAB_INCIDENTI off
 end
